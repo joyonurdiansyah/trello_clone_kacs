02df97df3ba642639bc353b317241919aef4771b
joyonurdiansyah <nurdiansyahjoyo@gmail.com>
2026-02-19 10:38:36 +0700

    feat feature services, match supabase db

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
+import { useBoards } from "@/lib/hooks/useBoards";
+import { useUser } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+
+export default function DashboardPage() {
+    const { user } = useUser();
+    const { createBoard } = useBoards()
+
+    const handleCreateBoard = async () => {
+        await createBoard({title: "New Board"});
+    }
+
+    // https://creative-bullfrog-43.clerk.accounts.dev
+
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <Navbar />
+
+            <main className="container mx-auto px-4 py-6 sm:py-8">
+                <div className="mb-6 sm:mb-8">
+                    <h1 className="text-2xl sm:text-3xl font-bold text-gray-900 mb-2">Welcome back, {user?.firstName ?? user?.emailAddresses[0]?.emailAddress}! 🐱</h1>
+                    <p className="text-gray-600">Heres whats happening with your boards and tasks today.</p>
+                    <Button className="w-full sm:w-auto" onClick={handleCreateBoard}>
+                        <Plus className="h-4 w-4 mr-2" />
+                        Create Board
+                    </Button>
+                </div>
+            </main>
+        </div>
+    );
+}
